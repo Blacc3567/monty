faea1945963b60a232ec090b373db2be296a65ca
root <Olatunde Pedro>
2023-01-19 11:36:57 -0800

    0

--- monty.h
+++ monty.h
@@ -1,33 +1,30 @@
 #ifndef MONTY_H
 #define MONTY_H
 
-#define _POSIX_C_SOURCE  200809L
-#define _GNU_SOURCE
-#include <stdio.h>
+#include <stddef.h>
 #include <stdlib.h>
-#include <string.h>
-#include <ctype.h>
-#include <unistd.h>
-#include <stdarg.h>
 
-#define UNDEFINED 0
-#define DELIM " \r\t\n"
-#define USAGE "USAGE: monty file\n"
-#define NOACCESS "Error: Can't open file %s\n"
-#define INVALID "L%d: unknown instruction %s\n"
-#define MALLOC "Error: malloc failed\n"
-#define PUSH "L%d: usage: push integer\n"
-#define POP "L%d: can't pop an empty stack\n"
-#define PINT "L%d: can't pint, stack empty\n"
-#define SWAP "L%d: can't swap, stack too short\n"
-#define ADD "L%d: can't add, stack too short\n"
-#define SUB "L%d: can't sub, stack too short\n"
-#define DIV "L%d: can't div, stack too short\n"
-#define DERR "L%d: division by zero\n"
-#define MUL "L%d: can't mul, stack too short\n"
-#define MOD "L%d: can't mod, stack too short\n"
-#define PCHAR "L%d: can't pchar, stack empty\n"
-#define RANGE "L%d: can't pchar, value out of range\n"
+#define INSTRUCTIONS              \
+	{                           \
+		{"push", push},       \
+		    {"pall", pall},   \
+		    {"pint", pint},   \
+		    {"pop", pop},     \
+		    {"swap", swap},   \
+		    {"nop", nop},     \
+		    {"div", _div},    \
+		    {"mul", _mul},    \
+		    {"add", _add},    \
+		    {"sub", _sub},    \
+		    {"mod", mod},     \
+		    {"pchar", pchar}, \
+		    {"pstr", pstr},   \
+		    {"rotl", rotl},   \
+		    {"rotr", rotr},   \
+		{                     \
+			NULL, NULL      \
+		}                     \
+	}
 
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
@@ -36,7 +33,7 @@
  * @next: points to the next element of the stack (or queue)
  *
  * Description: doubly linked list node structure
- * for stack, queues, LIFO, FIFO Holberton project
+ * for stack, queues, LIFO, FIFO
  */
 typedef struct stack_s
 {
@@ -51,7 +48,7 @@ typedef struct stack_s
  * @f: function to handle the opcode
  *
  * Description: opcode and its function
- * for stack, queues, LIFO, FIFO Alx project
+ * for stack, queues, LIFO, FIFO
  */
 typedef struct instruction_s
 {
@@ -60,51 +57,46 @@ typedef struct instruction_s
 } instruction_t;
 
 /**
- * struct global_s - global struct variable
- * @head: head of the stack
- * @value: value passed into the opcode
- * @line_number: line number of the opcode
- * @bufline: line read from bytecode
- * @fd: file descriptor
- * @cmd: opcode
- *
- */
-typedef struct global_s
+* struct help - argument for the current opcode
+* @data_struct: stack mode, stack (default) and queue
+* @argument: the arguments of the string
+*
+* Description: global structure used to pass data around the functions easily
+*/
+typedef struct help
 {
-	stack_t *head;
-	char *value;
-	unsigned int line_number;
-	char *bufline;
-	FILE *fd;
-	char *cmd;
-	int mode;
-	unsigned int idx;
-} global_t;
+	int data_struct;
+	char *argument;
+} help;
+help global;
 
-extern global_t var;
+/* stack utility functions available in linked_list.c */
+stack_t *add_node(stack_t **stack, const int n);
+stack_t *queue_node(stack_t **stack, const int n);
+void free_stack(stack_t *stack);
+size_t print_stack(const stack_t *stack);
 
-void free_var(void);
-void free_stack(void);
-void run_cmd(char *bufline);
-void (*get_cmd(void))(stack_t **head, unsigned int line_number);
-void erro(int code, ...);
-void erro_ext(int code, int va_arg);
-char *ivstg(char *operand, unsigned int line_number);
-void pusher(stack_t **stack, unsigned int line_number);
-void paller(stack_t **stack, unsigned int line_number);
-void pinter(stack_t **stack, unsigned int line_number);
-void popper(stack_t **stack, unsigned int line_number);
-void swapper(stack_t **stack, unsigned int line_number);
-void adder(stack_t **stack, unsigned int line_number);
-void subber(stack_t **stack, unsigned int line_number);
-void divider(stack_t **stack, unsigned int line_number);
-void multpler(stack_t **stack, unsigned int line_number);
-void modder(stack_t **stack, unsigned int line_number);
-void nopper(stack_t **stack, unsigned int line_number);
-void pchar(stack_t **stack, unsigned int line_number);
-void rotl(stack_t **stack, unsigned int line_number);
-void rotr(stack_t **stack, unsigned int line_number);
-void pstr(stack_t **stack, unsigned int line_number);
-void changemode(stack_t **stack, unsigned int line_number);
+void push(stack_t **stack, unsigned int line_cnt);
+void pall(stack_t **stack, unsigned int line_cnt);
+void pint(stack_t **stack, unsigned int line_cnt);
+void swap(stack_t **stack, unsigned int line_cnt);
+void pop(stack_t **stack, unsigned int line_cnt);
+void nop(stack_t **stack, unsigned int line_cnt);
 
-#endif
+void _div(stack_t **stack, unsigned int line_cnt);
+void _add(stack_t **stack, unsigned int line_cnt);
+void _sub(stack_t **stack, unsigned int line_cnt);
+void _mul(stack_t **stack, unsigned int line_cnt);
+void mod(stack_t **stack, unsigned int line_cnt);
+
+void pchar(stack_t **stack, unsigned int line_cnt);
+void pstr(stack_t **stack, unsigned int line_cnt);
+void rotl(stack_t **stack, unsigned int line_count);
+void rotr(stack_t **stack, unsigned int line_count);
+
+void opcode(stack_t **stack, char *str, unsigned int line_cnt);
+
+int is_digit(char *string);
+int isnumber(char *str);
+
+#endif /* MONTY_H */
